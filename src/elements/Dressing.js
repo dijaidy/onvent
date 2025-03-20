3f6f74a48d65222355396f660fb91e29e634c421
dijaidy <65172181+dijaidy@users.noreply.github.com>
2025-03-20 17:10:04 +0900

    escape setting

--- src/elements/Dressing.js
+++ src/elements/Dressing.js
@@ -4,6 +4,7 @@ import  "./Dressing.css";
 export default function Dressing() {
     const [stage, setStage] = useState(0);
     const [combi, setCombi] = useState([-1, -1, -1]);
+    const [combiFinished, setCombiFinished] = useState(false);
 
     const closet = [
         ['빨간옷', '파란옷', '노란옷'],
@@ -11,7 +12,7 @@ export default function Dressing() {
         ['빨간모자', '파란모자', '녹색모자']
     ]
     console.log(combi)
-    const a = 1;
+    
     return (
         <div>
             { 
@@ -30,9 +31,13 @@ export default function Dressing() {
             </div>:
             <div style={{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
             <text style={{fontSize: 15, textAlign: 'center', marginTop: 20, marginBottom: 20}} >리오 옷입히기</text>
-            <div className='RioImg'>
-
+            <div className='RioImg' style={{flexDirection: 'column', display: 'flex'}}>
+                {[0, 0, 0].map((_, i) => (combi[i] != -1 && <text key={'cloth'+ i.toString()}>{closet[i][combi[i]]}</text>))}
             </div>
+            {(combiFinished) ?
+            <text>완성</text>
+            :
+            <div>
             <div style={{flexDirection: 'row'}}>
                 <button onClick={()=>{setCombi(combi.map((cloth, idx)=> {return idx==stage-1 ? 0 : cloth} ))}}>
                     <text style={{fontSize: 15, textAlign: 'center', marginTop: 20, marginBottom: 20, marginRight: 10, marginLeft: 10}} >{closet[stage-1][0]}</text>
@@ -45,6 +50,7 @@ export default function Dressing() {
                 </button>
             </div>
             <div style={{flexDirection: 'row'}}>
+                { stage >= 2 &&
                 <button 
                 style={{backgroundColor: '#00ff00', borderWidth: 0, borderRadius: 5, marginTop: 20, width: 200, height: 30,}}
                 onClick={()=>{
@@ -52,14 +58,22 @@ export default function Dressing() {
                 }}>
                     <text>이전</text>
                 </button>
+}
                 <button 
                 style={{backgroundColor: '#00ff00', borderWidth: 0, borderRadius: 5, marginTop: 20, width: 200, height: 30,}}
-                onClick={()=>{
+                onClick={stage==3 ? 
+                    ()=>{
+                        setCombiFinished(true);
+                    }
+                    : 
+                    ()=>{
                     setStage(stage+1);
                 }}>
-                    <text>다음</text>
+                    <text>{stage==3 ? '완료' : '다음'}</text>
                 </button>
             </div>
+            </div>
+}
         </div>
             }
         </div>
--- src/elements/Escaping.js
+++ src/elements/Escaping.js
@@ -1,7 +1,123 @@
+import React, { useEffect, useRef, useState } from "react";
+import rio from '../asset/Doraemon.jpg';
+import { useDrag } from "react-use-gesture";
+import { useSpring, animated } from "react-spring";
+
+const screenWidth = 350;
+const screenHeight = 500;
+const rioWidth = 100;
+const rioHeight = 100;
+const joystickRadius = 50;
+const joystickInterRadius = 25;
+const velocity = 0.1;
+const barrier = [[40, 140, 50, 200], [140, 140, 50, 50]];
+
 export default function Escaping() {
+    const joystickPos = useSpring({x: 0, y: 0});
+    const rioPos = useSpring({x: 0, y: 0});
+    let animationId;
+
+    const updatePosition = () => {
+      const rioVelocity = [joystickPos.x.get(), joystickPos.y.get()];
+      let newX = Math.min(Math.max(rioPos.x.get() + rioVelocity[0]*velocity, 0), screenWidth-rioWidth);
+      let newY = Math.min(Math.max(rioPos.y.get() + rioVelocity[1]*velocity, 0), screenHeight-rioHeight);
+
+      let barrierLeft;
+      let barrierRight;
+      let barrierTop;
+      let barrierBottom;
+
+      for (let i = 0; i < barrier.length ; i++) {
+        barrierLeft = barrier[i][0];
+        barrierRight = barrier[i][0]+barrier[i][2];
+        barrierTop = barrier[i][1];
+        barrierBottom = barrier[i][1]+barrier[i][3];
+
+        if (barrierRight-10 < newX && newX < barrierRight&& newY + rioHeight > barrierTop && newY < barrierBottom) { // right collision
+          if (barrierTop+10 > newY + rioHeight &&  newY + rioHeight > barrierTop && newX + rioWidth > barrierLeft && newX < barrierRight && rioVelocity[1] >= 0){
+            newY = barrierTop - rioHeight;
+            break;
+          } else if (barrierBottom-10 < newY && newY < barrierBottom && newX + rioWidth > barrierLeft && newX < barrierRight && rioVelocity[1] <= 0) {
+            newY = barrierBottom;
+            break;
+          }
+          newX = barrierRight;
+          break;
+        } else if (barrierLeft+10 > newX + rioWidth && newX + rioWidth > barrierLeft && newY + rioHeight > barrierTop && newY < barrierBottom) { // left collision
+          if (barrierTop+10 > newY + rioHeight &&  newY + rioHeight > barrierTop && newX + rioWidth > barrierLeft && newX < barrierRight && rioVelocity[1] >= 0){
+            newY = barrierTop - rioHeight;
+            break;
+          } else if (barrierBottom-10 < newY && newY < barrierBottom && newX + rioWidth > barrierLeft && newX < barrierRight && rioVelocity[1] <= 0) {
+            newY = barrierBottom;
+            break;
+          }
+          newX = barrierLeft - rioWidth;
+          break;
+        } else if (barrierTop+10 > newY + rioHeight &&  newY + rioHeight > barrierTop && newX + rioWidth > barrierLeft && newX < barrierRight){ // top collision
+          newY = barrierTop - rioHeight;
+          break;
+        } else if (barrierBottom-10 < newY && newY < barrierBottom && newX + rioWidth > barrierLeft && newX < barrierRight){ // bottom collision
+          newY = barrierBottom;
+          break;
+        }
+
+      }
+
+        
+      rioPos.x.set(newX);
+      rioPos.y.set(newY);
+
+      animationId = requestAnimationFrame(updatePosition);
+    };
+
+    const bindLogoPos = useDrag((e)=>{
+      if (e.first){
+        joystickPos.x.set(0);
+        joystickPos.y.set(0);
+        updatePosition();
+      } else if (e.down){
+        // joystick setting
+        const dx = e.movement[0];
+        const dy = e.movement[1]; 
+        const distance = Math.sqrt(dx * dx + dy * dy); 
+
+        if (distance > joystickRadius-joystickInterRadius) {
+            const angle = Math.atan2(dy, dx); 
+            joystickPos.x.set(Math.cos(angle) * (joystickRadius-joystickInterRadius));
+            joystickPos.y.set(Math.sin(angle) * (joystickRadius-joystickInterRadius));
+        } else {
+            joystickPos.x.set(e.movement[0]);
+           joystickPos.y.set(e.movement[1]);
+        }
+
+
+      } else if (e.last) {
+        joystickPos.x.start(0, {config: {duration: 200}});
+        joystickPos.y.start(0, {config: {duration: 200}});
+        setTimeout(()=>{cancelAnimationFrame(animationId)}, 200);
+      }
+
+        },
+      {
+        preventScroll: true,
+        pointer: { touch: true},
+      })
+
+    const handleDragStart = (e) => {
+      e.preventDefault(); 
+    };
+
+
     return (
-        <div>
-            <h1>Escaping</h1>
+      <div style={{position: 'relative', width: screenWidth, height: screenHeight, borderWidth: 1, borderStyle: 'solid', borderColor: '#000000', touchAction: 'none'}}>
+        <animated.div style={{ x:rioPos.x, y:rioPos.y, width: 100, height: 100}}>
+            <img src={rio} onDragStart={handleDragStart} alt={'rio'} style={{width: rioWidth, height: rioHeight, }}></img>
+        </animated.div>
+        <div style={{display: 'flex', justifyContent: 'center', width: 100, height: 100, borderRadius: 50, backgroundColor: '#000000', opacity: 0.3, position: 'absolute', right: 50, bottom: 50}}>
+          <animated.div {...bindLogoPos()}  style={{width: 50, height: 50, x:joystickPos.x, y:joystickPos.y, borderRadius: 25, background: '#ffffff', alignSelf: 'center', pointerEvents: 'auto'}}>
+          </animated.div>
         </div>
-    )
-}
+        {barrier.map((pos, i)=><div key={'barrier'+i.toString()} style={{ position: 'absolute', left: pos[0], top: pos[1], width: pos[2], height: pos[3], borderColor: '#000000', borderWidth: 1, borderStyle: 'solid',}}></div>)}
+      </div>
+    );
+  }
