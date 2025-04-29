4a49eb6dfca596a139057237befce7a4fbfe4d39
dijaidy <65172181+dijaidy@users.noreply.github.com>
2025-04-30 06:02:14 +0900

    .

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./elements/MainPage";
 import Escaping from "./elements/Escaping";
-import Ranking from "./elements/Ranking";
 import Dressing from "./elements/Dressing2";
 
 
@@ -12,7 +11,6 @@ export default function Navigation() {
         <Routes>
             <Route path='/' element={<MainPage/>}/>
             <Route path='/escaping' element={<Escaping/>}/>
-            <Route path='/bithumb' element={<Ranking/>}/>
             <Route path='/dressing' element={<Dressing/>}/>
         </Routes>
     )
