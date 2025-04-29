5779b7fa1982e5b7aac05e68815ea2a5a8af664a
dijaidy <65172181+dijaidy@users.noreply.github.com>
2025-04-30 05:03:07 +0900

    bithumb

--- src/elements/Ranking.js
+++ src/elements/Ranking.js
@@ -1,7 +1,16 @@
+import { useNavigate } from "react-router-dom";
+import 빗썸화면 from "../asset/dressingImages/빗썸화면.svg"
+import 스프링클제목 from "../asset/dressingImages/스프링클제목.svg"
+import { rh, rw } from "../managements/Dimensions";
+
+const deviceRatio = window.innerHeight/window.innerWidth;
+
 export default function Ranking() {
+    const navigation = useNavigate();
     return (
-        <div>
-            <h1>Ranking</h1>
-        </div>
+        <button onClick={()=>{navigation('..')}} style={{backgroundColor: '#ee8aa8', borderWidth: 0, display: 'flex', width: '100%', height: window.innerHeight, justifyContent: 'flex-end', flexDirection: 'column'}}>
+            <img src={스프링클제목} style={{position: 'absolute', top: 0}} width={window.innerWidth}></img>
+            <img src={빗썸화면} height={Math.min(window.innerHeight - window.innerWidth*(342.02/489.84), (window.innerWidth-rw(40))*(665.7/450.04))} style={{marginBottom: rh(50)}}></img>
+        </button>
     )
 }
